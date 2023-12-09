a924007b29272b983f871fc44a2899322fa6005a
Tracy Miao <tracymiao98@gmail.com>
2023-12-08 22:19:44 -0500

    finish markov code

--- code/markov_chain.py
+++ code/markov_chain.py
@@ -1,11 +1,14 @@
 from __future__ import division
 import sys
 import random
-import os 
+import os
 import networkx as nx
 import util
 
-def approx_markov_chain_steady_state(conditional_distribution, N_samples, iterations_between_samples):
+
+def approx_markov_chain_steady_state(
+    conditional_distribution, N_samples, iterations_between_samples
+):
     """
     Computes the steady-state distribution by simulating running the Markov
     chain. Collects samples at regular intervals and returns the empirical
@@ -32,18 +35,38 @@ def approx_markov_chain_steady_state(conditional_distribution, N_samples, iterat
 
     # -------------------------------------------------------------------------
     # YOUR CODE GOES HERE FOR PART (a)
+    states = list(conditional_distribution.keys())
+    current_state = random.choice(states)
+    for _ in range(N_samples * iterations_between_samples):
+        # Decide whether to follow the original chain or move to a random state
+        if random.random() < 0.9:
+            # Follow the original chain's distribution
+            current_state = conditional_distribution[current_state].sample()
+        else:
+            # Transition to a random state
+            current_state = random.choice(states)
 
-
+        # Collect a sample at specified intervals
+        if _ % iterations_between_samples == 0:
+            empirical_distribution[current_state] += 1
+    empirical_distribution.normalize()
     # END OF YOUR CODE FOR PART (a)
     # -------------------------------------------------------------------------
 
     return empirical_distribution
 
+
 def get_graph_distribution(filename):
     G = nx.read_gml(filename)
     d = nx.to_dict_of_dicts(G)
-    cond_dist = util.Distribution({k: util.Distribution({k_: v_['weight'] for k_,v_ in v.items()}) for k,v in d.items()})
+    cond_dist = util.Distribution(
+        {
+            k: util.Distribution({k_: v_["weight"] for k_, v_ in v.items()})
+            for k, v in d.items()
+        }
+    )
     return cond_dist
+
 
 def run_pagerank(data_filename, N_samples, iterations_between_samples):
     """
@@ -67,22 +90,25 @@ def run_pagerank(data_filename, N_samples, iterations_between_samples):
     """
     conditional_distribution = get_graph_distribution(data_filename)
 
-    steady_state = approx_markov_chain_steady_state(conditional_distribution,
-                            N_samples,
-                            iterations_between_samples)
+    steady_state = approx_markov_chain_steady_state(
+        conditional_distribution, N_samples, iterations_between_samples
+    )
 
     pages = conditional_distribution.keys()
-    top = sorted( (((steady_state[page]), page) for page in pages), reverse=True )
+    top = sorted((((steady_state[page]), page) for page in pages), reverse=True)
 
     values_to_show = min(20, len(steady_state))
     print("Top %d pages from empirical distribution:" % values_to_show)
     for i in range(0, values_to_show):
-        print("%0.6f: %s" %top[i])
+        print("%0.6f: %s" % top[i])
     return steady_state
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python markovChain.py <data file> <samples> <iterations between samples>")
+        print(
+            "Usage: python markovChain.py <data file> <samples> <iterations between samples>"
+        )
         sys.exit(1)
     data_filename = sys.argv[1]
     N_samples = int(sys.argv[2])
